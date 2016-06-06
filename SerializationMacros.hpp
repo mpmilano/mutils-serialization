b375ac8438508c27e9e6235e9737c83337ed8ebf
Matthew Milano <milano@cs.cornell.edu>
2016-06-06 16:06:33 -0400

    more cases for default_serialization_support

--- SerializationMacros.hpp
+++ SerializationMacros.hpp
@@ -4,6 +4,13 @@
  * Hi! SKIP down to where you see the next comment.
  */
 
+//SERIALIZERS
+#define DEFAULT_SERIALIZE1(a) int to_bytes(char* ret) const {	\
+		return mutils::to_bytes(a,ret);							\
+	}															\
+	int bytes_size() const {									\
+		return mutils::bytes_size(a);							\
+	}
 #define DEFAULT_SERIALIZE2(a,b) int to_bytes(char* ret) const {	\
 		int sa = mutils::to_bytes(a,ret);						\
 		return sa + mutils::to_bytes(b,ret + sa);				\
@@ -20,6 +27,35 @@
 		return mutils::bytes_size(a) + mutils::bytes_size(b) + mutils::bytes_size(c); \
 	}
 
+#define DEFAULT_SERIALIZE4(a,b,c,d) int to_bytes(char* ret) const {		\
+		int sa = mutils::to_bytes(a,ret);								\
+		int sb = mutils::to_bytes(b,ret + sa);							\
+		int sc = mutils::to_bytes(b,ret + sa + sb);						\
+		return sa + sb + sc + mutils::to_bytes(d,ret + sa + sb + sc);	\
+	}																	\
+	int bytes_size() const {											\
+		return mutils::bytes_size(a) + mutils::bytes_size(b) + mutils::bytes_size(c) + mutils::bytes_size(d); \
+	}
+
+#define DEFAULT_SERIALIZE5(a,b,c,d,e) int to_bytes(char* ret) const {	\
+		int sa = mutils::to_bytes(a,ret);								\
+		int sb = mutils::to_bytes(b,ret + sa);							\
+		int sc = mutils::to_bytes(b,ret + sa + sb);						\
+		int sd = mutils::to_bytes(b,ret + sa + sb + sc);				\
+		return sa + sb + sc + + sd + mutils::to_bytes(d,ret + sa + sb + sc + sd); \
+	}																	\
+	int bytes_size() const {											\
+		return mutils::bytes_size(a) + mutils::bytes_size(b) + mutils::bytes_size(c) + mutils::bytes_size(d) + mutils::bytes_size(e); \
+	}
+
+
+//DESERIALIZERS
+#define DEFAULT_DESERIALIZE2(Name,a)									\
+	static std::unique_ptr<Name> from_bytes(mutils::DeserializationManager* p, char const * v){ \
+		auto a2 = mutils::from_bytes<std::decay_t<decltype(a)> >(p,v);	\
+		Name r{*a2};													\
+		return mutils::heap_copy(r);									\
+	}
 #define DEFAULT_DESERIALIZE3(Name,a,b)									\
 	static std::unique_ptr<Name> from_bytes(mutils::DeserializationManager* p, char const * v){		\
 		auto a2 = mutils::from_bytes<std::decay_t<decltype(a)> >(p,v);	\
@@ -34,21 +70,28 @@
 		Name r{*a2,*b2,*(mutils::from_bytes<std::decay_t<decltype(c)> >(p,v + size_a2 + mutils::bytes_size(*b2)))}; \
 		return mutils::heap_copy(r);									\
 	}
-
-#define DEFAULT_SERIALIZE1(a) int to_bytes(char* ret) const {	\
-		return mutils::to_bytes(a,ret);							\
-	}															\
-	int bytes_size() const {									\
-		return mutils::bytes_size(a);							\
-	}
-
-#define DEFAULT_DESERIALIZE2(Name,a)									\
-	static std::unique_ptr<Name> from_bytes(mutils::DeserializationManager* p, char const * v){ \
+#define DEFAULT_DESERIALIZE5(Name,a,b,c,d)								\
+	static std::unique_ptr<Name> from_bytes(mutils::DeserializationManager* p, char const * v){					\
 		auto a2 = mutils::from_bytes<std::decay_t<decltype(a)> >(p,v);	\
-		Name r{*a2};													\
+		auto size_a2 = mutils::bytes_size(*a2);							\
+		auto b2 = mutils::from_bytes<std::decay_t<decltype(b)> >(p,v + size_a2); \
+		auto size_b2 = mutils::bytes_size(*b2);							\
+		auto c2 = mutils::from_bytes<std::decay_t<decltype(c)> >(p,v + size_a2 + size_b2); \
+		Name r{*a2,*b2,*c2,*(mutils::from_bytes<std::decay_t<decltype(d)> >(p,v + size_a2 + size_b2 + mutils::bytes_size(*c2)))}; \
 		return mutils::heap_copy(r);									\
 	}
-
+#define DEFAULT_DESERIALIZE6(Name,a,b,c,d,e)								\
+	static std::unique_ptr<Name> from_bytes(mutils::DeserializationManager* p, char const * v){					\
+		auto a2 = mutils::from_bytes<std::decay_t<decltype(a)> >(p,v);	\
+		auto size_a2 = mutils::bytes_size(*a2);							\
+		auto b2 = mutils::from_bytes<std::decay_t<decltype(b)> >(p,v + size_a2); \
+		auto size_b2 = mutils::bytes_size(*b2);							\
+		auto c2 = mutils::from_bytes<std::decay_t<decltype(c)> >(p,v + size_a2 + size_b2); \
+		auto size_c2 = mutils::bytes_size(*c2);							\
+		auto d2 = mutils::from_bytes<std::decay_t<decltype(d)> >(p,v + size_a2 + size_b2 + size_c2); \
+		Name r{*a2,*b2,*c2,*d2,*(mutils::from_bytes<std::decay_t<decltype(e)> >(p,v + size_a2 + size_b2 + size_c2 + mutils::bytes_size(*d2)))}; \
+		return mutils::heap_copy(r);									\
+	}
 	
 #define DEFAULT_SERIALIZE_IMPL2(count, ...) DEFAULT_SERIALIZE ## count (__VA_ARGS__)
 #define DEFAULT_SERIALIZE_IMPL(count, ...) DEFAULT_SERIALIZE_IMPL2(count, __VA_ARGS__)
@@ -66,7 +109,7 @@
  * plop this macro inside the body of a class which extends 
  * ByteRepresentable, providing the name of the class (that you plopped this into)
  * as the first argument and the name of the class's fields as the remaining arguments.
- * Right now we only support up to two fields; adding more support is easy, just ask if
+ * Right now we only support up to five fields; adding more support is easy, just ask if
  * you need.
  *
  * MAJOR CAVEAT: This macro assumes that there is a constructor
